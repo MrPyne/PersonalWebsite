655210903b5106d91d892f9ba8b7956a130705da
matts <matt.pyne@hotmail.com>
2017-11-12 10:59:38 -0400

    Updated home message.

--- src/components/main/home.js
+++ src/components/main/home.js
@@ -56,22 +56,22 @@ export default () => (
     </div>
     <div style={styleBody}>
       <p>
-         Am quite bad at this kind of thing, talking about myself, it allways sounds
-         so corny, atleast to me it does. Anyways, what I think is important to know about
+         What I think is important to know about
          me is that I like to learn and build stuff. In my spare time am ussually
-         building something or programming and learning a hole bunch along the way.
+         building something or programming.
         <br />
         <br />
          My education is in electrical and computer engineering, recieving a BENG degree from
          DalHousie University in 2015. I have worked, since 2015, developing freely
-         available 3D CAD software, ProtocaseDesigner, at Protocaseas
-         Software Engineer.
+         available 3D CAD software, ProtocaseDesigner, at Protocases as a
+         Software Engineer. I have also done other things but the majority of time
+         has been at Protocase.
         <br />
         <br />
         Feel free to check out some of my projects and or other aspects about me.
         Try visiting my GitHub to see what software projects am working on.
         Please note, this site was my first attempt at building a site and as such
-        it is not the best but hopefully useable.
+        it is not the best but hopefully useable. Site is still under construction.
       </p>
     </div>
   </div>
